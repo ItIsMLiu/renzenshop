c3cb64cfe4eee7460766a28cc5c496ebf125a558
ItIsMLiu <mliu.design@outlook.com>
2025-03-03 18:02:24 +0000

    about, contact and home page code cleaned and optimised.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,106 +1,87 @@
 import React from 'react';
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Row, Col, Card, Form, Button } from "react-bootstrap";
 
 import "../style/Contact.css"
 import "../style/Global.css"
 
 function Contact() {
-  return <>
+  return (
+  <>
     <Container fluid className="genericHero contactImg d-flex flex-column justify-content-center align-items-center text-center">
-      <h1 className="fw-bold">Contact Us</h1>
-      <p className="fs-4">We'd love to hear from you! Whether you have a question, feedback, or need assistance, feel free to reach out to us.</p>
+      <h1>Contact Us</h1>
+      <p>We'd love to hear from you! Whether you have a question, feedback, or need assistance, feel free to reach out to us.</p>
     </Container>
 
     <Container fluid className="py-5">
       <Row className="justify-content-center">
-        <Col lg={6} md={8} sm={11}>
-          <Card className="py-4">
+        <Col lg={5} md={8} sm={11}>
+          {/* Contact Form */}
+          <Card className="py-4 px-3">
             <h2>Contact Form</h2>
-            <p>For your convenience, please use the form below to send us a message. We typically respond within 24 hours. If you don't receive a reply, please check your junk or spam folder, as some messages may be directed there. Thank you!
-            </p>
-            <form action="https://getform.io/f/axowwyrb" method="POST" id='contactForm'>
-              <div class="mb-3">
-                <label for="name" class="form-label">Name:</label>
-                <input type="text" name="name" class="form-control" id="name"></input>
-              </div>
-              <div class="mb-3">
-                <label for="email" class="form-label">Email address:</label>
-                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp"></input>
-                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-              </div>
-              <div class="mb-3">
-                <label for="message" class="form-label">Message:</label>
-                <textarea type="text" name="message" class="form-control" id="message"></textarea>
-              </div>
-              <button type="submit" class="btn btn-primary" id="contact">Submit</button>
-            </form>
-            <p>Thank you for reaching out to us. We look forward to assisting you!</p>
+            <p>We typically respond within 24 hours. If you don't see a reply, please check your junk or spam folder. Thank you!</p>
+
+            <Form action="https://getform.io/f/axowwyrb" method="POST">
+              <Form.Group className="mb-3">
+                <Form.Label>Name:</Form.Label>
+                <Form.Control type="text" name="name" required />
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Email Address:</Form.Label>
+                <Form.Control type="email" name="email" aria-describedby="emailHelp" required />
+                <Form.Text className="text-muted">We'll never share your email.</Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Message:</Form.Label>
+                <Form.Control as="textarea" name="message" rows={3} required />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">Submit</Button>
+            </Form>
           </Card>
 
-          <Card className="py-4">
+          {/* Get in Touch */}
+          <Card className="py-4 px-3 mt-4">
             <h2>Get in Touch</h2>
-            <p>We are based in Bristol and operate exclusively online. We serve Bristol and nearby cities, including Cardiff and Bath etc. Curtain samples can be brought to your home during scheduled measurement appointments.</p>
-            <ul className="nav nav-fill nav-underline">
-              <li className="nav-item">
-                <a className="nav-link icon-link" href="mailto:renzenshop@outlook.com">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-envelope" viewBox="0 0 16 16"><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/></svg>
-                  renzenshop@outlook.com
-                </a>
-              </li>
-              <li className="nav-item icon-link">
-                <a className="nav-link" href="https://wa.me/+447759721379?text=Hello%2C%20I%20need%20assistance%20with%20choosing%20new%20curtains." target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
-                  <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
-                </svg>
-                WhatsApp Chat
-                </a>
-              </li>
-              {/* <li className="nav-item icon-link">
-                <a className="nav-link" href="tel: 07759721379">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-phone" viewBox="0 0 16 16"><path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/><path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/></svg>
-                  07759721379
-                </a>
-              </li> */}
-              <li className="nav-item icon-link">
-                <a className="nav-link" href="https://www.google.com/maps/place/Bristol/@51.4683734,-2.7555235,11z/data=!3m1!4b1!4m6!3m5!1s0x4871836681b3d861:0x8ee4b22e4b9ad71f!8m2!3d51.454513!4d-2.58791!16zL20vMDk1bDA?entry=ttu" target="_blank">
-                  <span className="icon-container">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt" viewBox="0 0 16 16">
-                      <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
-                      <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                    </svg>
-                  </span>
-                  <span className="text-container">
-                    Bristol, UK
-                  </span>
-                </a>
-              </li>
-            </ul>
+            <p>Based in Bristol, we serve nearby cities, including Cardiff and Bath. Curtain samples are available during scheduled appointments.</p>
+            <div className="d-flex flex-column flex-sm-row gap-3">
+              <a href="mailto:renzenshop@outlook.com" className="custom-link d-flex gap-1 py-1">
+                <i className="bi bi-envelope"></i> renzenshop@outlook.com
+              </a>
+              <a href="https://wa.me/+447759721379?text=Hello%2C%20I%20need%20assistance%20with%20choosing%20new%20curtains" target="blank" className="custom-link d-flex gap-1 py-1">
+                <i className="bi bi-whatsapp"></i> WhatsApp Chat
+              </a>
+              {/*
+              <a href="tel: 07759721379" target="blank" className="custom-link d-flex gap-1 py-1">
+                <i className="bi bi-phone"></i> 07759721379
+              </a>
+              */}
+              <a href="https://www.google.com/maps/place/Bristol/@51.4683734,-2.7555235,11z/data=!3m1!4b1!4m6!3m5!1s0x4871836681b3d861:0x8ee4b22e4b9ad71f!8m2!3d51.454513!4d-2.58791!16zL20vMDk1bDA?entry=ttu" target="blank" className="custom-link d-flex gap-1 py-1">
+                <i className="bi bi-geo-alt"></i> Bristol, UK
+              </a>
+            </div>
           </Card>
 
-          <Card className="py-4">
-            <h2 className="card-title" id="git">Connect With Us</h2>
-            <p>Follow us on social media for updates and promotions:</p>
-            <ul className="nav nav-fill nav-underline">
-              <li className="nav-item icon-link">
-                <a className="nav-link" href="https://www.instagram.com/renzenshop/" target="_blank">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-instagram" viewBox="0 0 16 16"><path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/></svg>    
-                  Instagram
-                </a>
-              </li>
-              <li className="nav-item icon-link">
-                <a className="nav-link" href="https://www.facebook.com/people/RenZen-Shop/61550549634449/" target="_blank">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                    <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
-                  </svg>  
-                  Facebook
-                </a>
-              </li>
-            </ul>
+          {/* Social Media */}
+          <Card className="py-4 px-3 mt-4">
+            <h2>Connect With Us</h2>
+            <p>Follow us for updates and promotions:</p>
+
+            <div className="d-flex flex-column flex-sm-row gap-3">
+              <a href="https://www.instagram.com/renzenshop/" className="custom-link d-flex gap-1 py-1" target="blank">
+                <i className="bi bi-instagram"></i> Instagram
+              </a>
+              <a href="https://www.facebook.com/people/RenZen-Shop/61550549634449/" target="blank" className="custom-link d-flex gap-1 py-1">
+                <i className="bi bi-facebook"></i> Facebook
+              </a>
+            </div>          
           </Card>
         </Col>
       </Row>
     </Container>
   </>
+  )
 }
 
 export default Contact;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Container, Button } from 'react-bootstrap';
 
-import "../style/Home.css"
-import "../style/Global.css"
+import "../style/Home.css";
+import "../style/Global.css";
 
 function Home() {
-  return <>
-    <div className="mb-4">
-      <div className="container-fluid homeHero">
-        <p className="col-12 fs-4">Made to Measure</p>
-        <h1 className="col-12 fw-bold">Premium Curtains</h1>
-        <p className="col-12 fs-4">Style your space, frame your view.</p>
-        <NavLink 
-          to="/renzenshop/contact"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          <button className="btn btn-primary btn-lg" type="button" id="herogit">Get in Touch!</button>
-        </NavLink>
-        
-      </div>
-    </div>
-    
-  </>
+  return (
+    <Container fluid className="homeHero">
+      <p>Made to Measure</p>
+      <h1>Premium Curtains</h1>
+      <p>Style your space, frame your view.</p>
+      <NavLink to="/renzenshop/contact" end>
+        <Button variant="primary" size="lg" id="herogit">
+          Get in Touch!
+        </Button>
+      </NavLink>
+    </Container>
+  );
 }
 
 export default Home;
